83beff5ebad1bf7bddc13f9a022d1e1b338e5621
Serkan Demirören <demirorens@gmail.com>
2022-05-13 17:30:10 +0300

    Vault integrated for secret management.

---
+++ config-vault/config/vault-init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VAULT_DEV_TOKEN=c9858719-8008-4564-8d24-5afb6db6ef98
+
+vault login ${VAULT_DEV_TOKEN}
+
+vault secrets disable secret
+vault secrets enable -version=1 -path=secret kv
+vault kv put secret/notebook-service @${CONFIG_DIR}/database.json
+vault kv put secret/note-service @${CONFIG_DIR}/database.json
+vault kv put secret/tag-service @${CONFIG_DIR}/database.json
+vault kv put secret/oauth2-server @${CONFIG_DIR}/keycloak.json
+vault kv put secret/user-service @${CONFIG_DIR}/user-service.json
+
