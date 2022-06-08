acc71e4692a2e27bce92ede76b71998bd07c5fb6
Serkan Demirören <demirorens@gmail.com>
2022-06-09 00:02:31 +0300

    docker-compose related fixes.

--- config-vault/config/vault-init-docker.sh
+++ config-vault/config/vault-init-docker.sh
@@ -8,9 +8,20 @@ vault secrets disable secret
 vault secrets enable -version=1 -path=secret kv
 
 vault kv put secret/application @${CONFIG_DIR}/rabbitmq.json
+vault kv put secret/application/docker @${CONFIG_DIR}/rabbitmq.json
+
 vault kv put secret/notebook-service @${CONFIG_DIR}/database.json
+vault kv put secret/notebook-service/docker @${CONFIG_DIR}/database.json
+
 vault kv put secret/note-service @${CONFIG_DIR}/database.json
+vault kv put secret/note-service/docker @${CONFIG_DIR}/database.json
+
 vault kv put secret/tag-service @${CONFIG_DIR}/database.json
+vault kv put secret/tag-service/docker @${CONFIG_DIR}/database.json
+
 vault kv put secret/oauth2-server @${CONFIG_DIR}/keycloak.json
+vault kv put secret/oauth2-server/docker @${CONFIG_DIR}/keycloak.json
+
 vault kv put secret/user-service @${CONFIG_DIR}/user-service.json
+vault kv put secret/user-service/docker @${CONFIG_DIR}/user-service.json
 
